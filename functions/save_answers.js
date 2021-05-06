8bc40eeb883269738e2fa7c68b4d1251e9c692c6
Cindy Qi Li <cli@ocadu.ca>
2021-05-06 16:58:53 -0400

    fix: fix the issue with losing previously submitted data

--- functions/save_answers.js
+++ functions/save_answers.js
@@ -1,18 +1,17 @@
 "use strict";
 
 const {Octokit} = require("@octokit/core");
-const axios = require("axios");
 const uuid = require("uuid");
 
 const githubAPI = "https://api.github.com";
 
 /**
  * Check if an aswer file in the given branch exists. If exists, along with the existence flag, return
- * `download_url` and `sha` of this file that will be used at the file update.
+ * `content` and `sha` of this file that will be used at the file update.
  * @param {Object} octokit An instance of octokit with authentication set.
  * @param {String} branch The name of the branch the file existence to be checked against.
  * @return {Promise} The resolved prmoise contains an object with a boolean flag keyed by `exists`. If
- * the file exists. The returned object contains keys `download_url` and `sha` of the existing file.
+ * the file exists. The returned object contains keys `content` and `sha` of the existing file.
  */
 const checkAnswerFileExists = async (octokit, branch) => {
 	return new Promise((resolve, reject) => {
@@ -24,8 +23,8 @@ const checkAnswerFileExists = async (octokit, branch) => {
 		}).then((response) => {
 			resolve({
 				exists: true,
-				download_url: response.data.download_url,
-				sha: response.data.sha
+				content: JSON.parse(Buffer.from(response.data.content, "base64").toString("utf-8")),
+				sha: response.data.sha,
 			});
 		}).catch((e) => {
 			if (e.message === "Not Found") {
@@ -75,38 +74,34 @@ const createAnswerFile = async (octokit, branch, newAnswer) => {
 /**
  * Update an existing answers file by appending the new answer with a uuid key to the exising file.
  * @param {Object} octokit An instance of octokit with authentication set.
- * @param {String} downloadURL The download URL of the existing file.
+ * @param {String} jsonFileContent The content of the existing answers.json file.
  * @param {String} sha The sha of the existing file.
  * @param {String} branch The name of the branch the file will be updated.
  * @param {Object} newAnswer The answer appended into the existing file.
  * @return {Promise} The resolve or reject of the promise indicate the file is updated successfully or
  * unsuccessfully.
  */
-const updateAnswerFile = async (octokit, downloadURL, sha, branch, newAnswer) => {
+const updateAnswerFile = async (octokit, jsonFileContent, sha, branch, newAnswer) => {
 	const uniqueId = uuid.v4();
-	return new Promise((resolve, reject) => {
-		// fetch the existing answers file
-		axios.get(downloadURL).then((downloadResponse) => {
-			const jsonFileContent = downloadResponse.data;
-			jsonFileContent[uniqueId] = newAnswer;
+	jsonFileContent[uniqueId] = newAnswer;
 
-			// Update an existing answers file
-			octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-			    owner: process.env.WORDLES_REPO_OWNER,
-			    repo: process.env.WORDLES_REPO_NAME,
-			    path: "src/_data/answers.json",
-				// Including "[skip ci]" in the commit message notifies Netlify not to trigger a deploy.
-				message: "chore: [skip ci] update answers.json",
-				content: Buffer.from(JSON.stringify(jsonFileContent)).toString("base64"),
-				sha: sha,
-			    branch: branch
-		    }).then(() => {
-				resolve();
-			})
-			.catch((e) => {
-				reject("Error at updating the answers file: ", e.message)
-			})
-		});
+	return new Promise((resolve, reject) => {
+		// Update an existing answers file
+		octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+		    owner: process.env.WORDLES_REPO_OWNER,
+		    repo: process.env.WORDLES_REPO_NAME,
+		    path: "src/_data/answers.json",
+			// Including "[skip ci]" in the commit message notifies Netlify not to trigger a deploy.
+			message: "chore: [skip ci] update answers.json",
+			content: Buffer.from(JSON.stringify(jsonFileContent)).toString("base64"),
+			sha: sha,
+		    branch: branch
+	    }).then(() => {
+			resolve();
+		})
+		.catch((e) => {
+			reject("Error at updating the answers file: ", e.message)
+		})
 	});
 };
 
@@ -134,15 +129,16 @@ exports.handler = async function(event, context, callback) {
 	};
 
 	try {
-		// check if answers.json exits
+		// check if answers.json exists
 		const answerFileInfo = await checkAnswerFileExists(octokit, incomingData["branch"]);
 
 		if (answerFileInfo.exists) {
 			// File exists: update the existing answers file
 			console.log("Updating an existing answers file...");
-			await updateAnswerFile(octokit, answerFileInfo.download_url, answerFileInfo.sha, incomingData["branch"], newAnswer);
+			await updateAnswerFile(octokit, answerFileInfo.content, answerFileInfo.sha, incomingData["branch"], newAnswer);
 			console.log("Done: Updated the answers file.");
 		} else {
+			// File does not exist: create a new answers file
 			console.log("Creating a new answers file...");
 			await createAnswerFile(octokit, incomingData["branch"], newAnswer);
 			console.log("Done: Created the answers file.");
@@ -153,6 +149,7 @@ exports.handler = async function(event, context, callback) {
 			body: "Success"
 		};
 	} catch (e) {
+		console.log("Error: ", e);
 		return {
 			statusCode: 400,
 			body: JSON.stringify({
