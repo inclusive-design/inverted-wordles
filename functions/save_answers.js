b7779042d3119fc89c66876df65ce875e8e9bfa3
Cindy Qi Li <cli@ocadu.ca>
2021-05-06 15:19:17 -0400

    fix: improve the code and style the answer question page

--- functions/save_answers.js
+++ functions/save_answers.js
@@ -6,86 +6,158 @@ const uuid = require("uuid");
 
 const githubAPI = "https://api.github.com";
 
-const octokit = new Octokit({
-	auth: process.env.ACCESS_TOKEN
-});
-
-exports.handler = async function(event, context, callback) {
-	const incomingData = JSON.parse(event.body);
-	const uniqueId = uuid.v4();
-
-	// Reject the request when:
-	// 1. Not a POST request;
-	// 2. Doesn’t provide required values
-	if (event.httpMethod !== "POST" || !incomingData["branch"] || !incomingData["answers"]) {
-		callback(null, {
-			statusCode: 400,
-			body: "Invalid HTTP request method or missing field values."
-		});
-	} else {
-		let isError = false, errorMsg;
-		const newAnswer = {
-			answers: incomingData["answers"],
-			createdTimestamp: new Date().toISOString()
-		};
-
-		// check if answers.json exits
+/**
+ * Check if an aswer file in the given branch exists. If exists, along with the existence flag, return
+ * `download_url` and `sha` of this file that will be used at the file update.
+ * @param {Object} octokit An instance of octokit with authentication set.
+ * @param {String} branch The name of the branch the file existence to be checked against.
+ * @return {Promise} The resolved prmoise contains an object with a boolean flag keyed by `exists`. If
+ * the file exists. The returned object contains keys `download_url` and `sha` of the existing file.
+ */
+const checkAnswerFileExists = async (octokit, branch) => {
+	return new Promise((resolve, reject) => {
 		octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+			owner: process.env.WORDLES_REPO_OWNER,
+			repo: process.env.WORDLES_REPO_NAME,
+			path: "src/_data/answers.json",
+			ref: branch
+		}).then((response) => {
+			resolve({
+				exists: true,
+				download_url: response.data.download_url,
+				sha: response.data.sha
+			});
+		}).catch((e) => {
+			if (e.message === "Not Found") {
+				resolve({
+					exists: false
+				});
+			} else {
+				reject("Error at checking the existence of the answers file: " + e.message)
+			}
+
+		});
+	});
+};
+
+/**
+ * Create a new answers file. The new answer is written into the file in JSON and keyed by a uuid.
+ * @param {Object} octokit An instance of octokit with authentication set.
+ * @param {String} branch The name of the branch the file will be created.
+ * @param {Object} newAnswer The answer written into the new file.
+ * @return {Promise} The resolve or reject of the promise indicate the file is created successfully or
+ * unsuccessfully.
+ */
+const createAnswerFile = async (octokit, branch, newAnswer) => {
+	const uniqueId = uuid.v4();
+	return new Promise((resolve, reject) => {
+		const jsonFileContent = {};
+		jsonFileContent[uniqueId] = newAnswer;
+
+		// Create a new answers file
+		octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
 		    owner: process.env.WORDLES_REPO_OWNER,
 		    repo: process.env.WORDLES_REPO_NAME,
 		    path: "src/_data/answers.json",
-		    ref: incomingData["branch"]
-		}).then((fileExistenceResponse) => {
-			// File exists: update the existing answers file
-			console.log("Updating an existing answers file...");
+			// Including "[skip ci]" in the commit message notifies notifies Netlify not to trigger a deploy.
+			message: "chore: [skip ci] create answers.json",
+			content: Buffer.from(JSON.stringify(jsonFileContent)).toString("base64"),
+		    branch: branch
+	    }).then(() => {
+			resolve();
+		})
+		.catch((e) => {
+			reject("Error at creating the answers file: ", e.message);
+		})
+	});
+};
 
-			// fetch the existing answers file
-			axios.get(fileExistenceResponse.data.download_url).then((downloadResponse) => {
-				const jsonFileContent = downloadResponse.data;
-				jsonFileContent[uniqueId] = newAnswer;
-
-				// Update an existing answers file
-				octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-				    owner: process.env.WORDLES_REPO_OWNER,
-				    repo: process.env.WORDLES_REPO_NAME,
-				    path: "src/_data/answers.json",
-					// Including "[skip ci]" in the commit message notifies Netlify not to trigger a deploy.
-					message: "chore: [skip ci] update answers.json",
-					content: new Buffer(JSON.stringify(jsonFileContent)).toString("base64"),
-					sha: fileExistenceResponse.data.sha,
-				    branch: incomingData["branch"]
-			    }).catch((error) => {
-					isError = true;
-					errorMsg = "Error at creating the answers file."
-				})
-				console.log("Done: Updated the answers file.");
-			});
-		}).catch(() => {
-			// File doesn't exist: create an answers file
-			console.log("Creating a new answers file...");
-
-			const jsonFileContent = {};
+/**
+ * Update an existing answers file by appending the new answer with a uuid key to the exising file.
+ * @param {Object} octokit An instance of octokit with authentication set.
+ * @param {String} downloadURL The download URL of the existing file.
+ * @param {String} sha The sha of the existing file.
+ * @param {String} branch The name of the branch the file will be updated.
+ * @param {Object} newAnswer The answer appended into the existing file.
+ * @return {Promise} The resolve or reject of the promise indicate the file is updated successfully or
+ * unsuccessfully.
+ */
+const updateAnswerFile = async (octokit, downloadURL, sha, branch, newAnswer) => {
+	const uniqueId = uuid.v4();
+	return new Promise((resolve, reject) => {
+		// fetch the existing answers file
+		axios.get(downloadURL).then((downloadResponse) => {
+			const jsonFileContent = downloadResponse.data;
 			jsonFileContent[uniqueId] = newAnswer;
 
-			// Create a new answers file
+			// Update an existing answers file
 			octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
 			    owner: process.env.WORDLES_REPO_OWNER,
 			    repo: process.env.WORDLES_REPO_NAME,
 			    path: "src/_data/answers.json",
-				// Including "[skip ci]" in the commit message notifies notifies Netlify not to trigger a deploy.
-				message: "chore: [skip ci] create answers.json",
-				content: new Buffer(JSON.stringify(jsonFileContent)).toString("base64"),
-			    branch: incomingData["branch"]
-		    }).catch((error) => {
-				isError = true;
-				errorMsg = "Error at creating the answers file."
+				// Including "[skip ci]" in the commit message notifies Netlify not to trigger a deploy.
+				message: "chore: [skip ci] update answers.json",
+				content: Buffer.from(JSON.stringify(jsonFileContent)).toString("base64"),
+				sha: sha,
+			    branch: branch
+		    }).then(() => {
+				resolve();
 			})
-			console.log("Done: Created the answers file.");
+			.catch((e) => {
+				reject("Error at updating the answers file: ", e.message)
+			})
 		});
+	});
+};
 
-		callback(null, {
-			statusCode: isError ? 500 : 200,
-			body: errorMsg ? errorMsg : "Success."
-		});
+exports.handler = async function(event, context, callback) {
+	const incomingData = JSON.parse(event.body);
+
+	// Reject the request when:
+	// 1. Not a POST request;
+	// 2. Doesn’t provide required values
+	if (event.httpMethod !== "POST" || !incomingData["branch"] || !incomingData["answers"] ||
+	!process.env.ACCESS_TOKEN || !process.env.WORDLES_REPO_OWNER || !process.env.WORDLES_REPO_NAME) {
+		return {
+			statusCode: 400,
+			body: "Invalid HTTP request method or missing field values or missing environment variables."
+		};
+	}
+
+	// Main process
+	const octokit = new Octokit({
+		auth: process.env.ACCESS_TOKEN
+	});
+	const newAnswer = {
+		answers: incomingData["answers"],
+		createdTimestamp: new Date().toISOString()
+	};
+
+	try {
+		// check if answers.json exits
+		const answerFileInfo = await checkAnswerFileExists(octokit, incomingData["branch"]);
+
+		if (answerFileInfo.exists) {
+			// File exists: update the existing answers file
+			console.log("Updating an existing answers file...");
+			await updateAnswerFile(octokit, answerFileInfo.download_url, answerFileInfo.sha, incomingData["branch"], newAnswer);
+			console.log("Done: Updated the answers file.");
+		} else {
+			console.log("Creating a new answers file...");
+			await createAnswerFile(octokit, incomingData["branch"], newAnswer);
+			console.log("Done: Created the answers file.");
+		}
+
+		return {
+			statusCode: 200,
+			body: "Success"
+		};
+	} catch (e) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+		        error: e
+	      	})
+		};
 	}
 };
