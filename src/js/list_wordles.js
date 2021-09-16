7e5452795d007468dec2666b510df6a741f2539d
Cindy Qi Li <cli@ocadu.ca>
2021-09-16 15:45:03 -0400

    fix: fix bugs when populating wordles at the page log

--- src/js/list_wordles.js
+++ src/js/list_wordles.js
@@ -1,8 +1,8 @@
 "use strict";
 
-/* global globalOptions, inverted_wordles */
+/* global inverted_wordles */
 
-inverted_wordles.listWordles = function (wordles, wordlesAreaSelector, generalStatusSelector) {
+inverted_wordles.listWordles = function (wordles, wordlesAreaSelector, createButtonSelector, generalStatusSelector) {
     const generalStatusElm = document.querySelector(generalStatusSelector);
 
     fetch("/api/check_deploy", {
@@ -24,47 +24,44 @@ inverted_wordles.listWordles = function (wordles, wordlesAreaSelector, generalSt
         } else {
             response.json().then(res => {
                 let wordlesHtml = document.querySelector(wordlesAreaSelector).innerHTML;
-                let branchesInDeploy = [];
+                let branchesInDeploy = {};
 
-                // Add all branches that have been deployed to the wordle list
+                // Loop through all wordles to separate ones that have been deployed or in the progress of deploy
                 for (const [branchName, questionFile] of Object.entries(wordles)) {
-                    const workshopName = questionFile.exists && questionFile.content.workshopName ? inverted_wordles.escapeHtml(questionFile.content.workshopName) : "";
-                    const question = questionFile.exists && questionFile.content.question ? inverted_wordles.escapeHtml(questionFile.content.question) : "";
-                    const entries = questionFile.exists && questionFile.content.entries ? questionFile.content.entries : "";
-                    const lastModifiedTimestamp = questionFile.exists && questionFile.content.lastModifiedTimestamp ? questionFile.content.lastModifiedTimestamp.substring(0, 10).replace(/-/g, "/") : "";
+                    const escapedQuestionData = {};
+                    escapedQuestionData.workshopName = questionFile.exists && questionFile.content.workshopName ? inverted_wordles.escapeHtml(questionFile.content.workshopName) : "";
+                    escapedQuestionData.question = questionFile.exists && questionFile.content.question ? inverted_wordles.escapeHtml(questionFile.content.question) : "";
+                    escapedQuestionData.entries = questionFile.exists && questionFile.content.entries ? questionFile.content.entries : "";
+                    escapedQuestionData.lastModifiedTimestamp = questionFile.exists && questionFile.content.lastModifiedTimestamp ? questionFile.content.lastModifiedTimestamp.substring(0, 10).replace(/-/g, "/") : "";
                     if (res[branchName]) {
-                        inverted_wordles.renderWordleRow({
+                        wordlesHtml += inverted_wordles.renderWordleRow({
                             branchName,
-                            workshopName,
-                            question,
-                            entries,
-                            lastModifiedTimestamp,
+                            workshopName: escapedQuestionData.workshopName,
+                            question: escapedQuestionData.question,
+                            entries: escapedQuestionData.entries,
+                            lastModifiedTimestamp: escapedQuestionData.lastModifiedTimestamp,
                             disableInputs: false,
                             isCreateNew: false
                         });
                     } else {
-                        branchesInDeploy.push(branchName);
+                        branchesInDeploy[branchName] = escapedQuestionData;
                     }
                 }
+                // Add all branches that have been deployed to the wordle list
                 document.querySelector(wordlesAreaSelector).innerHTML = wordlesHtml;
 
                 // Append branches that are in the process of deploy to the wordle list
-                for (const branchName in branchesInDeploy) {
-                    inverted_wordles.renderWordleRow({
+                for (const [branchName, questionFile] of Object.entries(branchesInDeploy)) {
+                    inverted_wordles.appendInDeployWordleRow(wordlesAreaSelector, {
                         branchName,
-                        workshopName: res[branchName].workshopName,
-                        question: res[branchName].question,
-                        entries: res[branchName].entries,
-                        lastModifiedTimestamp: res[branchName].lastModifiedTimestamp,
-                        statusMsg: "*Please wait until the question link is generated and webpage is created. This may take 30 seconds*",
-                        extraStatusClass: "purple",
-                        extraRowClass: "grey-background",
-                        disableInputs: true,
-                        isCreateNew: true
+                        workshopName: questionFile.workshopName,
+                        question: questionFile.question,
+                        entries: questionFile.entries,
+                        lastModifiedTimestamp: questionFile.lastModifiedTimestamp
                     });
 
                     // Bind the polling event to update the wordle row when the deploy is ready
-                    inverted_wordles.bindPolling(globalOptions.selectors.wordlesArea, globalOptions.selectors.createButton, {
+                    inverted_wordles.bindPolling(wordlesAreaSelector, createButtonSelector, {
                         branchName,
                         workshopName: res[branchName].workshopName,
                         question: res[branchName].question,
